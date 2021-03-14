033575e1eacadc6bcdc9f9be4bfe4d3d6d8c4721
thodnett <taniahodnett@yahoo.ie>
2021-03-14 13:38:01 +0000

    Added comments

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ URL = "http://f0ae7213ef73.eu.ngrok.io/"
 
 ENDPOINT="/webservice/rest/server.php"
 
+#Code provided by local_wsmanagesections plugin
 def rest_api_parameters(in_args, prefix='', out_dict=None):
     """Transform dictionary/array structure to a flat dictionary, with key names
     defining the structure.
@@ -37,6 +38,7 @@ def rest_api_parameters(in_args, prefix='', out_dict=None):
             rest_api_parameters(item, prefix.format(key), out_dict)
     return out_dict
 
+#Code provided by local_wsmanagesections plugin
 def call(fname, **kwargs):
     """Calls moodle API function with function name fname and keyword arguments.
     Example:
@@ -51,12 +53,13 @@ def call(fname, **kwargs):
         raise SystemError("Error calling Moodle API\n", response)
     return response
 
+#Code provided by local_wsmanagesections plugin
 class LocalGetSections(object):
     """Get settings of sections. Requires courseid. Optional you can specify sections via number or id."""
     def __init__(self, cid, secnums = [], secids = []):
         self.getsections = call('local_wsmanagesections_get_sections', courseid = cid, sectionnumbers = secnums, sectionids = secids)
 
-
+#Code provided by local_wsmanagesections plugin
 class LocalUpdateSections(object):
     """Updates sectionnames. Requires: courseid and an array with sectionnumbers and sectionnames"""
     def __init__(self, cid, sectionsdata):
